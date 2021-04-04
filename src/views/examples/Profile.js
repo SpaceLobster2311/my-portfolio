62c8535c471eb02050db869fc3451f83317c09aa
Alexander Williams <alex.williams2311@gmail.com>
2021-04-03 18:35:25 -0700

    lab-05 complete

--- src/views/examples/Profile.js
+++ src/views/examples/Profile.js
@@ -17,13 +17,15 @@
 */
 import React from "react";
 
+// big import
+
 // reactstrap components
-import { Button, Card, Container, Row, Col } from "reactstrap";
+import { Button, Badge, Card, CardBody, Container, Row, Col } from "reactstrap";
 
 // core components
 import DemoNavbar from "components/Navbars/DemoNavbar.js";
 import SimpleFooter from "components/Footers/SimpleFooter.js";
-import profile from "assets/img/theme/team-4-800x800.jpg";
+import profile from "assets/img/theme/Headshot.jpeg";
 
 class Profile extends React.Component {
   componentDidMount() {
@@ -108,59 +110,56 @@ class Profile extends React.Component {
                     <Col className="order-lg-1" lg="4">
                       <div className="card-profile-stats d-flex justify-content-center">
                         <div>
-                          <span className="heading">22</span>
-                          <span className="description">Friends</span>
+                          <span className="heading">4</span>
+                          <span className="description">Years wasted on Politcal Science degree</span>
                         </div>
                         <div>
-                          <span className="heading">10</span>
-                          <span className="description">Photos</span>
+                          <span className="heading">1</span>
+                          <span className="description">Game(s) Created</span>
                         </div>
                         <div>
-                          <span className="heading">89</span>
-                          <span className="description">Comments</span>
+                          <span className="heading">2</span>
+                          <span className="description">Pets Owned</span>
                         </div>
                       </div>
                     </Col>
                   </Row>
                   <div className="text-center mt-5">
                     <h3>
-                      Jessica Jones{" "}
-                      <span className="font-weight-light">, 27</span>
+                      Alexander Williams{" "}
+                      <span className="font-weight-light">, 28</span>
                     </h3>
                     <div className="h6 font-weight-300">
                       <i className="ni location_pin mr-2" />
-                      Bucharest, Romania
+                      Seattle, Washington
                     </div>
                     <div className="h6 mt-4">
                       <i className="ni business_briefcase-24 mr-2" />
-                      Solution Manager - Creative Tim Officer
+                      Software Developer
                     </div>
                     <div>
                       <i className="ni education_hat mr-2" />
-                      University of Computer Science
+                      University of Washington
                     </div>
                   </div>
                   <div className="mt-5 py-5 border-top text-center">
                     <Row className="justify-content-center">
                       <Col lg="9">
                         <p>
-                          An artist of considerable range, Ryan — the name taken
-                          by Melbourne-raised, Brooklyn-based Nick Murphy —
-                          writes, performs and records all of his own music,
-                          giving it a warm, intimate feel with a solid groove
-                          structure. An artist of considerable range.
+                          I am a Software Developer based out of Seattle, WA. I have experience building apps in React using Javascript, CSS, and HTML.  I am building my own video game in GameMakerStudio 2 using GML.  I am excited to express my creativity in tech, and strive to create new and innovating experiences for users!
                         </p>
+
                         <a href="#pablo" onClick={e => e.preventDefault()}>
                           Show more
                         </a>
                       </Col>
                     </Row>
-                  </div>
+                  </div>                
                 </div>
-              </Card>
+              </Card>             
             </Container>
-          </section>
-        </main>
+          </section>         
+        </main>        
         <SimpleFooter />
       </>
     );
